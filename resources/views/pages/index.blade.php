ecff521b64519e6cf45ac304721fbc7734230d04
enli0r <milanniko19@gmail.com>
2021-10-27 18:46:33 +0200

    restyled homepage and improved breakpoints

--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -2,7 +2,7 @@
     <x-navbar />
     <x-category-navbar />
 
-    <div class="header container-parent">
+    <div class="header">
         <div class="container">
                 <div class="col">
                     <h1>give your<br>workout<br>a new style!</h1>
@@ -13,26 +13,29 @@
     </div>
 
     <!--Featured categories-->
-    <div class="featured-categories fp-row container-parent">
+    <section class="featured-categories">
         <div class="container">
-            <div class="row-title">
+            <div class="section-title">
                 <h2>Featured categories</h2>
                 <div id="rectangle"></div>
             </div>
-            <div class="row">
-                <div class="col-4">
+            <div class="featured-categories-wrapper grid-container">
+
+                <div class="grid-item">
                     <a href=""><img src="images/category-1.jpg" alt=""></a>
                     <div class="basic-info">
                         <h5>Casual</h5>
                     </div>
                 </div>
-                <div class="col-4">
+
+                <div class="grid-item">
                     <a href=""><img src="images/category-2.jpg" alt=""></a>
                     <div class="basic-info">
                         <h5>Lifestyle</h5>
                     </div>
                 </div>
-                <div class="col-4">
+
+                <div class="grid-item">
                     <a href=""><img src="images/category-3.jpg" alt=""></a>
                     <div class="basic-info">
                         <h5>Sport</h5>
@@ -40,78 +43,89 @@
                 </div>
             </div>
         </div>
-    </div>
+    </section>
 
     <!--Featured products-->
-    <div class="featured-products fp-row container-parent">
+    <section class="featured-products">
         <div class="container">
-            <div class="row-title">
+
+            <div class="section-title">
                 <h2>featured products</h2>
                 <div id="rectangle"></div>
             </div>
-            <div class="row">
-                <div class="col-3">
+
+            <div class="featured-products-wrapper grid-container">
+
+                <div class="grid-item">
                     <a href=""><img src="images/product-1.jpg" alt=""></a>
                     <div class="basic-info">
                         <h5>Red T-Shirt</h5>
                         <p>$30.00</p>
                     </div>
                 </div>
-                <div class="col-3">
+
+                <div class="grid-item">
                     <a href=""><img src="images/product-2.jpg" alt=""></a>
                     <div class="basic-info">
                         <h5>Sneakers</h5>
                         <p>$100.00</p>
                     </div>
                 </div>
-                <div class="col-3">
+
+                <div class="grid-item">
                     <a href=""><img src="images/product-3.jpg" alt=""></a>
                     <div class="basic-info">
                         <h5>Sweatsuit</h5>
                         <p>$60.00</p>
                     </div>
                 </div>
-                <div class="col-3">
+
+                <div class="grid-item">
                     <a href=""><img src="images/product-4.jpg" alt=""></a>
                     <div class="basic-info">
                         <h5>Blue T-Shirt</h5>
                         <p>$50.00</p>
                     </div>
                 </div>
+
             </div>
         </div>
-    </div>
+    </section>
 
     <!--Latest products-->
-    <div class="latest-products fp-row container-parent">
+    <section class="latest-products">
         <div class="container">
-            <div class="row-title">
+            <div class="section-title">
                 <h2>latest products</h2>
                 <div id="rectangle"></div>
             </div>
-            <div class="row ">
-                <div class="col-3">
+            <div class="latest-products-wrapper grid-container">
+
+                <div class="grid-item">
                     <a href=""><img src="images/product-5.jpg" alt=""></a>
                     <div class="basic-info">
                         <h5>Grey shoes</h5>
                         <p>$80.00</p>
                     </div>
                 </div>
-                <div class="col-3">
+
+                <div class="grid-item">
                     <a href=""><img src="images/product-6.jpg" alt=""></a>
                     <div class="basic-info">
                         <h5>Puma T-Shirt</h5>
                         <p>$40.00</p>
                     </div>
                 </div>
-                <div class="col-3">
+
+                <div class="grid-item">
                     <a href=""><img src="images/product-7.jpg" alt=""></a>
                     <div class="basic-info">
                         <h5>Socks</h5>
                         <p>$5.00</p>
                     </div>
                 </div>
-                <div class="col-3">
+
+                <div class="grid-item">
                     <a href=""><img src="images/product-8.jpg" alt=""></a>
                     <div class="basic-info">
                         <h5>Watch</h5>
@@ -120,10 +134,10 @@
                 </div>
             </div>
         </div>
-    </div>
+    </section>
 
     <!--Exclusive offer-->
-    <div class="exclusive-offer container-parent">
+    <section class="exclusive-offer container-parent">
         <div class="container">
             <div class="row">
                 <div class="col col-6">
@@ -138,71 +152,68 @@
                 </div>
             </div>
         </div>
-    </div>
+    </section>
 
     <!--Testimonial-->
-
-    <div class="testimonial container-parent">
+    <section class="testimonial">
         <div class="container">
-            <div class="row">
-                <div class="col-3">
-                    <div class="col-content">
-                        <i class="fa fa-quote-left" aria-hidden="true">&#x201C;</i>
-                        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Laboriosam, quod. Est excepturi reiciendis sed minus tempore ad quas hic ipsam, exercitationem quos, laboriosam natus consequuntur totam esse quibusdam cum at.</p>
-                        <img src="images/user-1.png" alt="">
-                        <div class="user-name">
-                            <h3>Caitlyn</h3>
-                            <h3>Orvel</h3>
-                        </div>
+            <div class="testimonial-wrapper grid-container">
+
+                <div class="grid-item">
+                    <i class="fa fa-quote-left" aria-hidden="true">&#x201C;</i>
+                    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Laboriosam, quod. Est excepturi reiciendis sed minus tempore ad quas hic ipsam, exercitationem quos, laboriosam natus consequuntur totam esse quibusdam cum at.</p>
+                    <img src="images/user-1.png" alt="">
+                    <div class="user-name">
+                        <h3>Caitlyn</h3>
+                        <h3>Orvel</h3>
                     </div>
                 </div>
-                <div class="col-3">
-                    <div class="col-content">
-                        <i class="fa fa-quote-left" aria-hidden="true">&#x201C;</i>
-                        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Laboriosam, quod. Est excepturi reiciendis sed minus tempore ad quas hic ipsam, exercitationem quos, laboriosam natus consequuntur totam esse quibusdam cum at.</p>
-                        <img src="images/user-2.png" alt="">
-                        <div class="user-name">
-                            <h3>Theodore</h3>
-                            <h3>Smith</h3>
-                        </div>
+
+                <div class="grid-item">
+                    <i class="fa fa-quote-left" aria-hidden="true">&#x201C;</i>
+                    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Laboriosam, quod. Est excepturi reiciendis sed minus tempore ad quas hic ipsam, exercitationem quos, laboriosam natus consequuntur totam esse quibusdam cum at.</p>
+                    <img src="images/user-2.png" alt="">
+                    <div class="user-name">
+                        <h3>Theodore</h3>
+                        <h3>Smith</h3>
                     </div>
                 </div>
-                <div class="col-3">
-                    <div class="col-content">
-                        <i class="fa fa-quote-left" aria-hidden="true">&ldquo;</i>
-                        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Laboriosam, quod. Est excepturi reiciendis sed minus tempore ad quas hic ipsam, exercitationem quos, laboriosam natus consequuntur totam esse quibusdam cum at.</p>
-                        <img src="images/user-3.png" alt="">
-                        <div class="user-name">
-                            <h3>Jennifer</h3>
-                            <h3>Lawrence</h3>
-                        </div>
+
+                <div class="grid-item">
+                    <i class="fa fa-quote-left" aria-hidden="true">&ldquo;</i>
+                    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Laboriosam, quod. Est excepturi reiciendis sed minus tempore ad quas hic ipsam, exercitationem quos, laboriosam natus consequuntur totam esse quibusdam cum at.</p>
+                    <img src="images/user-3.png" alt="">
+                    <div class="user-name">
+                        <h3>Jennifer</h3>
+                        <h3>Lawrence</h3>
                     </div>
                 </div>
+
             </div>
         </div>
-    </div>
+    </section>
 
     <!--Brands-->
-    <div class="brands container-parent">
+    <section class="brands">
         <div class="container">
-            <div class="row">
-                <div class="col-2">
+            <div class="brands-wrapper grid-container">
+                <div class="grid-item">
                     <img src="images/logo-godrej.png" alt="">
                 </div>
-                <div class="col-2">
+                <div class="grid-item">
                     <img src="images/logo-oppo.png" alt="">
                 </div>
-                <div class="col-2">
+                <div class="grid-item">
                     <img src="images/logo-coca-cola.png" alt="">
                 </div>
-                <div class="col-2">
+                <div class="grid-item">
                     <img src="images/logo-paypal.png" alt="">
                 </div>
-                <div class="col-2">
+                <div class="grid-item">
                     <img src="images/logo-philips.png" alt="">
                 </div>
             </div>
         </div>
-    </div>
+    </section>
     <x-footer />
 </x-layout>
