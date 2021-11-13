990b1af47fb5085da23535bcb7fff7a25fcb70cb
enli0r <milanniko19@gmail.com>
2021-11-13 22:21:51 +0100

    cleaned up code

--- resources/views/pages/products.blade.php
+++ resources/views/pages/products.blade.php
@@ -1,71 +1,32 @@
 <x-layout>
+    <section class="products">
+        <div class="container">
 
-    <!-- Section only appears if there are some child categories -->
-    {{-- @if (count($child_categories) > 0)
-
-        <section class="categories">
-            <div class="container">
-                <div class="section-title">
-                    <h2>Categories</h2>
-                </div>
-
-                <hr>
-
-                <div class="categories-wrapper grid-container">
-                    @foreach ($child_categories as $category)
-
-                        <div class="grid-item">
-                            <h1>{{ $category->name }}</h1>
-
-                            @if ($category->image != null)
-                                <img src="{{ $category->image }}" alt="">
-                            @else
-                                <img src="https://i.ibb.co/0JKP8r6/category-1.jpg" alt="">
-
-                            @endif
-                        </div>
-
-                    @endforeach
-                </div>
+            <div class="section-title">
+                <h2>Products</h2>
             </div>
-        </section>
 
-    @endif --}}
+            <hr>
 
- 
-        <section class="products">
-            <div class="container">
+            @if (count($products) > 0)
 
-                <div class="section-title">
-                    <h2>Products</h2>
-                </div>
+                <div class="products-wrapper grid-container">
+                    @foreach ($products as $product)
+                        <a href="{{ route('show', $product->id) }}">
+                            <div class="grid-item">
+                                <img class="product-image" src="{{ $product->images->first()->url }}" alt="">
+                                
+                                <div class="separator"></div>
 
-                <hr>
-
-                @if (count($products) > 0)
-
-                    <div class="products-wrapper grid-container">
-                        @foreach ($products as $product)
-                            <a href="{{ route('show', $product->id) }}">
-                                <div class="grid-item">
-                                    <img class="product-image" src="{{ $product->images->first()->url }}" alt="">
-                                    
-                                    <div class="separator"></div>
-
-                                    <div class="product-info">
-                                        <h5 class="product-name">{{ $product->name }}</h5>
-                                        <p class="product-price">${{ $product->price }}</p>
-                                    </div>
+                                <div class="product-info">
+                                    <h5 class="product-name">{{ $product->name }}</h5>
+                                    <p class="product-price">${{ $product->price }}</p>
                                 </div>
-                            </a>
-                        @endforeach      
-                    </div>
-                @endif
-            </div>
-        </section>
-
-        
-
-        
-
+                            </div>
+                        </a>
+                    @endforeach      
+                </div>
+            @endif
+        </div>
+    </section>
 </x-layout>
--- resources/views/pages/show.blade.php
+++ resources/views/pages/show.blade.php
@@ -1,78 +1,76 @@
 <x-layout>
-        <section class="product">
+    <section class="product">
+        <div class="container">
+            <div class="product-wrapper grid-container">
+                <img class="grid-item product-image" src="{{ $product->images->first()->url }}" alt="">
 
-            <div class="container">
-                <div class="product-wrapper grid-container">
-                    <img class="grid-item product-image" src="{{ $product->images->first()->url }}" alt="">
+                <div class="product-details grid-item">
+                    <h2 class="name">{{ $product->name }}</h2>
+                    <h2 class="code"><span>code</span>: {{ $product->code }}</h2>
 
-                    <div class="product-details grid-item">
-                        <h2 class="name">{{ $product->name }}</h2>
-                        <h2 class="code"><span>code</span>: {{ $product->code }}</h2>
+                    <div class="avaliability">
+                        <ul>
+                            @if ($product->quantity > 0)
 
-                        <div class="avaliability">
-                            <ul>
-                                @if ($product->quantity > 0)
+                                <li id="avaliable">Avaliable</li>     
 
-                                    <li id="avaliable">Avaliable</li>     
+                            @else
 
-                                @else
+                                <li id="not-avaliable">Not avaliable</li>
 
-                                    <li id="not-avaliable">Not avaliable</li>
+                            @endif
+                        </ul>
+                    </div>
+                    
 
+                    <div class="price">
+                        <h3 class="store-price">${{ $product->price }}</h3>
+                    
+                        <h3 class="online-price">${{ $product->online_price }}</h3>
+
+                        <p>Listed price is only for online purchase!</p>
+                    </div>
+
+                    <hr>
+
+                    @foreach ($attributes as $attribute)
+                        <p>{{ $attribute->name }}</p>
+
+                        <!-- cycling through attribute products associated with product -->
+                        @foreach ($attributeProducts as $attributeProduct) 
+                            <!-- cycling through attributeValues associated with current attribute through pivot table -->
+                            @foreach ($attributeProduct->attributeValues()->where('attribute_id', $attribute->id)->get() as $attributeValue)
+
+                            <!-- creating s/m/l symbols using first letter, it will change latter-->
+                            @php
+                                $sizeSymbol = substr($attributeValue->value, 0,1);
+                            @endphp
+
+                            <!-- setting div class based on attribute and its value -->
+                            <div class="{{ $attribute->code }} {{ $attributeValue->value }}">
+
+                                <!-- adding symbol only to size attribute -->
+                                @if ($attribute->code == 'size')
+                                    <p class="size-symbol">{{ $sizeSymbol }}</p>
                                 @endif
-                            </ul>
-                        </div>
+
+                            </div>
+
+                                
+                            @endforeach
+                        @endforeach
                         
+                    @endforeach
+                    
+                </div>
 
-                        <div class="price">
-                            <h3 class="store-price">${{ $product->price }}</h3>
-                       
-                            <h3 class="online-price">${{ $product->online_price }}</h3>
-
-                            <p>Listed price is only for online purchase!</p>
-                        </div>
-
-                        <hr>
-
-                       @foreach ($attributes as $attribute)
-                           <p>{{ $attribute->name }}</p>
-
-                           <!-- cycling through attribute products associated with product -->
-                           @foreach ($attributeProducts as $attributeProduct) 
-                                <!-- cycling through attributeValues associated with current attribute through pivot table -->
-                               @foreach ($attributeProduct->attributeValues()->where('attribute_id', $attribute->id)->get() as $attributeValue)
-
-                                <!-- creating s/m/l symbols using first letter, it will change latter-->
-                                @php
-                                    $sizeSymbol = substr($attributeValue->value, 0,1);
-                                @endphp
-
-                                <!-- setting div class based on attribute and its value -->
-                                <div class="{{ $attribute->code }} {{ $attributeValue->value }}">
-
-                                    <!-- adding symbol only to size attribute -->
-                                    @if ($attribute->code == 'size')
-                                        <p class="size-symbol">{{ $sizeSymbol }}</p>
-                                    @endif
-
-                                </div>
-
-                                   
-                               @endforeach
-                           @endforeach
-                           
-                       @endforeach
-                        
-                    </div>
-
-                    <div class="product-description grid-item">
-                        <h3>Product description</h3>
-                        <hr>
-                        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Iure quisquam maiores placeat eligendi? Libero odio omnis dolorem et, itaque quasi consequatur sint earum. Officia cupiditate illum nobis quae, consectetur itaque.</p>
-                    </div>
+                <div class="product-description grid-item">
+                    <h3>Product description</h3>
+                    <hr>
+                    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Iure quisquam maiores placeat eligendi? Libero odio omnis dolorem et, itaque quasi consequatur sint earum. Officia cupiditate illum nobis quae, consectetur itaque.</p>
                 </div>
             </div>
-
-        </section>        
+        </div>
+    </section>        
     <x-footer />
 </x-layout>
