6c59c5c9776465d2aeeb88467a606ce81f6b2751
enli0r <milanniko19@gmail.com>
2021-11-21 16:47:23 +0100

    extracted dropdown to component

--- resources/views/components/category-navbar.blade.php
+++ resources/views/components/category-navbar.blade.php
@@ -6,22 +6,36 @@
 
             <a href="{{ route('products') }}" class="category-link">All products</a>
 
-            <div class="dropdown">
+            <x-dropdown buttonName="Men" :categories="$categories" gender="men"/>
+            <x-dropdown buttonName="Women" :categories="$categories" gender="women"/>
+
+            {{-- <div class="dropdown">
                 <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton1" data-bs-toggle="dropdown" aria-expanded="false">
                   Men
                 </button>
 
-                <ul class="dropdown-menu" aria-labelledby="dropdownMenuButton2">
+                <ul class="dropdown-menu" aria-labelledby="dropdownMenuButton1">
                     
                     @foreach ($categories as $category)
 
                         @if (count($category->children) > 0)
                             
                             <ul>
-                                <li class="category-name"><a href="{{ route('products.categories', $category->slug) }}?gender=men">{{ $category->name }}</a></li>
+                                <form action="{{ route('products.categories', $category->slug) }}" method="POST">
+                                    @csrf
+
+                                    <input type="hidden" name="_method" value="PUT">
+                                    <button type="submit" name="gender" value="men" class="category-name">{{ $category->name }}</button>
+                                </form>
+
                                 
                                 @foreach ($category->children as $subcategory)
-                                    <li><a class="dropdown-item" href="{{ route('products.categories', [$category->slug, $subcategory->slug]) }}?gender=men">{{ $subcategory->name }}</a></li>
+                                    <form action="{{ route('products.categories', [$category->slug, $subcategory->slug]) }}" method="POST">
+                                        @csrf
+
+                                        <input type="hidden" name="_method" value="PUT">
+                                        <button type="submit" name="gender" value="men" class="dropdown-item">{{ $subcategory->name }}</button>
+                                    </form>
                                 @endforeach
                             </ul>
                         @endif
@@ -51,10 +65,8 @@
                             </ul>
                         @endif
                     @endforeach 
-
-                    
                 </ul>
-            </div>
+            </div> --}}
             
         
         </div>
---
+++ resources/views/components/dropdown.blade.php
@@ -0,0 +1,36 @@
+@props(['buttonName', 'categories', 'gender'])
+
+<div class="dropdown">
+    <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton1" data-bs-toggle="dropdown" aria-expanded="false">
+      {{ $buttonName }}
+    </button>
+
+    <ul class="dropdown-menu" aria-labelledby="dropdownMenuButton1">
+        
+        @foreach ($categories as $category)
+
+            @if (count($category->children) > 0)
+                
+                <ul>
+                    <form action="{{ route('products.categories', $category->slug) }}" method="POST">
+                        @csrf
+
+                        <input type="hidden" name="_method" value="PUT">
+                        <button type="submit" name="gender" value="{{ $gender }}" class="category-name">{{ $category->name }}</button>
+                    </form>
+
+                    
+                    @foreach ($category->children as $subcategory)
+                        <form action="{{ route('products.categories', [$category->slug, $subcategory->slug]) }}" method="POST">
+                            @csrf
+
+                            <input type="hidden" name="_method" value="PUT">
+                            <button type="submit" name="gender" value="{{ $gender }}" class="dropdown-item">{{ $subcategory->name }}</button>
+                        </form>
+                    @endforeach
+                </ul>
+            @endif
+        @endforeach 
+        
+    </ul>
+</div>
