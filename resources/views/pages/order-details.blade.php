6b32a699c7046b724fbfa133da09a04eb14ea7d3
enli0r <milanniko19@gmail.com>
2021-11-29 00:51:56 +0100

    added old values on order details form

--- resources/views/pages/order-details.blade.php
+++ resources/views/pages/order-details.blade.php
@@ -20,12 +20,12 @@
                     <div class="form-group name-surname">
                         <div class="input-label">
                             <label for="name">Name</label>
-                            <input  type="text" id="name" name="name">
+                            <input  type="text" id="name" name="name" value="{{old('name')}}">
                         </div>
 
                         <div class="input-label">
                             <label for="surname">Surname</label>  
-                            <input  type="text" id="surname" name="surname">
+                            <input  type="text" id="surname" name="surname" value="{{old('surname')}}">
                         </div>
 
                         
@@ -34,7 +34,7 @@
                     <div class="form-group">
                         <div class="input-label">
                             <label for="country">Country</label>
-                            <input  type="text" id="country" name="country">
+                            <input  type="text" id="country" name="country" value="{{old('country')}}">
                         </div>
                     </div>
                     
@@ -42,26 +42,26 @@
                     <div class="form-group city-zip-code">
                         <div class="input-label input-label-city">
                             <label for="city">City</label>
-                            <input  type="text" id="city" name="city">
+                            <input  type="text" id="city" name="city" value="{{old('city')}}">
                         </div>      
                     
                         <div class="input-label input-label-zip">
                             <label for="zip-code">Zip-code</label>
-                            <input  type="text" id="zip-code" name="zipCode">
+                            <input  type="text" id="zip-code" name="zipCode" value="{{old('zipCode')}}">
                         </div>
                     </div>
 
                     <div class="form-group">
                         <div class="input-label">
                             <label for="adress">Adress</label>
-                            <input type="text" id="adress" name="adress">
+                            <input type="text" id="adress" name="adress" value="{{old('adress')}}">
                         </div>
                     </div>
 
                     <div class="form-group">
                         <div class="input-label">
                             <label for="phone-number">Phone number</label>
-                            <input type="text" id="phone-number" name="phoneNumber">
+                            <input type="text" id="phone-number" name="phoneNumber" value="{{old('phoneNumber')}}">
                         </div>
                         
                     </div>
@@ -75,12 +75,12 @@
                         
                         <div class="input-label">
                             <label for="card">Card</label>
-                            <input type="radio" id="card" name="payment" value="card">
+                            <input type="radio" id="card" name="payment" value="card" @if(old('payment') == 'card') checked @endif>
                         </div>
                         
                         <div class="input-label">
                             <label for="on-delivery">On delivery</label>
-                            <input type="radio" id="on-delivery" name="payment" value="on-delivery">
+                            <input type="radio" id="on-delivery" name="payment" value="on-delivery" @if(old('payment') == 'on-delivery') checked @endif>
                         </div>
     
                     </div>
