b2b951b1b7feb884c5a79805bd773a158225a0e8
enli0r <milanniko19@gmail.com>
2021-12-06 19:05:38 +0100

    edited layout on products page

--- resources/views/pages/products.blade.php
+++ resources/views/pages/products.blade.php
@@ -1,15 +1,21 @@
 <x-layout>
     <section class="products">
-        <div class="container products-container">
-        <x-filter-sidebar :previouspost="$previouspost" :currentcategory="$currentcategory"/>
+        <div class="filters-products-wrapper">
+            <x-filter-sidebar :previouspost="$previouspost" :currentcategory="$currentcategory"/>
 
-            @if (count($products) > 0)
-                <div class="products-wrapper grid-container @if (count($products) < 3) grid-left-align @endif">
-                    @foreach ($products as $product)
-                        <x-product :product="$product"/>
-                    @endforeach      
-                </div>
-            @endif
+            <div class="products-container">
+
+                @if (count($products) > 0)
+                    <div class="products-wrapper grid-container @if (count($products) < 3) grid-left-align @endif">
+                        @foreach ($products as $product)
+                            <x-product :product="$product"/>
+                        @endforeach      
+                    </div>
+                @endif
+            </div>
+        
+        
+            
 
         </div>
     </section>
