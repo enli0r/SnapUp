d0a394cb1cf5ac479c4e55489842a8055137ec78
enli0r <milanniko19@gmail.com>
2021-12-07 17:21:21 +0100

    resized products

--- resources/views/components/product.blade.php
+++ resources/views/components/product.blade.php
@@ -1,14 +1,11 @@
 @props(['product'])
 
-<a href="{{ route('show', $product->id) }}">
-    <div class="grid-item">
-        <img class="product-image" src="{{ $product->images->first()->url }}" alt="">
-        
-        <div class="separator"></div>
+<a  class="grid-item"href="{{ route('show', $product->id) }}">
+    <img class="product-image" src="{{ $product->images->first()->url }}" alt="">
 
-        <div class="product-info">
-            <h5 class="product-name">{{ $product->name }}</h5>
-            <p class="product-price">${{ $product->price }}</p>
-        </div>
+    <div class="product-info">
+        <h5 class="product-name">{{ $product->name }}</h5>
+        <p class="product-price">${{ $product->price }}</p>
     </div>
 </a>
+
--- resources/views/pages/products.blade.php
+++ resources/views/pages/products.blade.php
@@ -1,21 +1,16 @@
 <x-layout>
     <section class="products">
-        <div class="filters-products-wrapper">
+        <div class="container filters-products-wrapper">
             <x-filter-sidebar :previouspost="$previouspost" :currentcategory="$currentcategory"/>
 
-            <div class="products-container">
 
-                @if (count($products) > 0)
-                    <div class="products-wrapper grid-container @if (count($products) < 3) grid-left-align @endif">
-                        @foreach ($products as $product)
-                            <x-product :product="$product"/>
-                        @endforeach      
-                    </div>
-                @endif
-            </div>
-        
-        
-            
+            @if (count($products) > 0)
+                <div class="products-wrapper grid-container @if (count($products) < 3) grid-left-align @endif">
+                    @foreach ($products as $product)
+                        <x-product :product="$product"/>
+                    @endforeach      
+                </div>
+            @endif
 
         </div>
     </section>
