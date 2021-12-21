946fba9c6c38dbf811e2726a91ba90626fd00729
enli0r <milanniko19@gmail.com>
2021-12-21 08:59:06 +0100

    added focus on search input

--- public/js/app.js
+++ public/js/app.js
@@ -37420,6 +37420,7 @@ $(function () {
 $(function () {
   $('.fa-search').on('click', function (event) {
     $('.search').toggleClass('search-clicked');
+    $('.search form input').focus();
   });
   $('.fa-times').on('click', function (event) {
     $('.search').removeClass('search-clicked');
--- resources/js/navbar.js
+++ resources/js/navbar.js
@@ -1,6 +1,7 @@
 $(function(){
     $('.fa-search').on('click', function(event){
         $('.search').toggleClass('search-clicked');
+        $('.search form input').focus();
     });
 
     $('.fa-times').on('click', function(event){
